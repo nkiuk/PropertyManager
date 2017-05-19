664fda70cccdacd3a622bc5b384aa55a04a9eb1b
noelk <noel.kiuk@hotmail.com>
2017-05-19 16:24:11 +1000

    PMSP first committ

---
+++ PMSP/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PMSP.Startup))]
+namespace PMSP
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
